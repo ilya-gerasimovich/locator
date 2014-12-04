31b3c192bc375d9f291fb6501ff1208c8e15529d
ilya-gerasimovich <gerasimovich.ilya@inbox.ru>
2014-12-05 00:48:04 +0400

    final style

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ window.onload = function(){
 	var sideBarRight = document.getElementById("sideRight");
 	var sessionWia = wialon.core.Session.getInstance();
 	var map = initMap();
-	var bounds = [];
-
 
 	LogIn.onclick = function(){
 		var login = loginInput.value;
@@ -22,74 +20,44 @@ window.onload = function(){
 			var session = sessionWia;
 			session.initSession('https://trc-api.wialon.com');
 			session.login(login,pass,'',function(code){
-				if(code == 0)
-				{
-					overlay.style.display = "none";
-					var name = session.getCurrUser().getName();
-					welcome.innerHTML = 'Hello, '+name;
-					logoutInput.style.display = "block";
-					session.loadLibrary('itemIcon');
-					session.updateDataFlags([{type: 'type', data: 'avl_unit', flags: 0x00000411, mode: 0}],function(){
-						var items = session.getItems("avl_unit");
-						var array = [];
-						var bounds = [];
-						for(var i = 0; i < items.length; i++)
-						{	
-						
-							if(items[i].getPosition() != null)
-							{
-								var date = wialon.util.DateTime.formatTime(items[i].getPosition().t);
-								container += "<div class='cont' unit='unit_"+items[i].getId()+"'>"+"<img src="+
-								items[i].getIconUrl(64)+
-								">"+"<span class='spanName'> "+
-								items[i].getName()+"</span>"+
-								"<br> Speed: "+items[i].getPosition().s+" km/h"+
-								"<br> Last date: "+date+
-								"<br> <span class='spanPos'>X: "+
-								items[i].getPosition().x+
-								"<br> Y: "+items[i].getPosition().y+
-								"</span></div>";
-
-								var myIcon = L.icon({
-								    iconUrl: items[i].getIconUrl(64),
-								    iconRetinaUrl: items[i].getIconUrl(64),
-								    iconSize: [32, 32],
-								    iconAnchor: [-16, -16],
-								    popupAnchor: [32, 16]
-								});
-
-
-								var marker = L.marker([items[i].getPosition().y, items[i].getPosition().x],{icon: myIcon}).addTo(map);
-								array[i] = L.latLng(items[i].getPosition().y, items[i].getPosition().x);
-
-								marker.bindPopup("<div>"+"<span class='spanName'> "+
-								items[i].getName()+"</span>"+
-								"<br> Speed: "+items[i].getPosition().s+" km/h"+
-								"<br> Last date: "+date+
-								"<br> <span class='spanPos'>X: "+
-								items[i].getPosition().x+
-								"<br> Y: "+items[i].getPosition().y+
-								"</span></div>").openPopup();
-							}
-							else
-								container += "<div class='cont'>"+"<img src="+
-								items[i].getIconUrl()+">"+
-								"<span class='spanName'> "+items[i].getName()+
-								"</span><br> Last date: unknown"+
-								"</div>";
+			if(code == 0)
+			{
+				var name = session.getCurrUser().getName();
+				overlay.style.display = "none";
+				welcome.innerHTML = 'Hello, '+ name;
+				logoutInput.style.display = "block";
+				session.loadLibrary('itemIcon');
+				session.updateDataFlags([{type: 'type', data: 'avl_unit', flags: 0x00000411, mode: 0}],function(){
+					var items = session.getItems("avl_unit");
+					var array = [];
+					for(var i = 0; i < items.length; i++)
+					{	
+						if(items[i].getPosition() != null)
+						{
+							container += addItems(items[i]);
+							var myIcon = addIcon(items[i]);
+							var marker = L.marker([items[i].getPosition().y, items[i].getPosition().x],{icon: myIcon}).addTo(map);
+							array[i] = L.latLng(items[i].getPosition().y, items[i].getPosition().x);
+							marker.bindPopup(popUp(items[i])).openPopup();
 						}
-					map.fitBounds(array);
-					map.setView(new L.LatLng(array[1].lat,array[1].lng));
-					console.log(array[1]);
-					sideBarLeft.innerHTML = container;
-					container = "";
-					});
-				}
-				else
-				{
-					passInput.value = "";
-					alert("Invalid login or password");
-				}
+						else
+							container += "<div class='cont'>"+"<img src="+
+							items[i].getIconUrl()+">"+
+							"<span class='spanName'> "+items[i].getName()+
+							"</span><br> Last date: unknown"+
+							"</div>";
+					}
+				map.fitBounds(array);
+				map.setView(new L.LatLng(array[0].lat,array[0].lng));
+				sideBarLeft.innerHTML = container;
+				container = "";
+				});
+			}
+			else
+			{
+				passInput.value = "";
+				alert("Invalid login or password");
+			}
 			});
 		}
 		else
@@ -106,6 +74,7 @@ window.onload = function(){
 				overlay.style.display = "block";
 				passInput.value = "";
 				logoutInput.style.display = "none";
+				sideBarLeft.innerHTML = "";
 			}
 		});
 	}
@@ -123,18 +92,55 @@ window.onload = function(){
 			id = id.split("_");
 			var item = sessionWia.getItem(id[1]);
 			map.setView(new L.LatLng(item.getPosition().y,item.getPosition().x),18);
-			map.fitBounds(L.latLngBounds(item.getPosition().y,item.getPosition().x));
-			//console.log(item);
 		}
 	}
 };
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
 function initMap() {
 	var map = L.map('map').setView([51.505, -0.09], 13);
 	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
+	var osm = new L.TileLayer(osmUrl);//, {attribution: osmAttrib});
 	map.addLayer(osm);
 	map.setView(new L.LatLng(51.3, 0.7),9);
 	return map;
+}
+
+function addItems(item) {
+	var date = wialon.util.DateTime.formatTime(item.getPosition().t);
+	var container = "<div class='cont' unit='unit_"+item.getId()+"'>"+"<img src="+
+	item.getIconUrl(64)+
+	">"+"<span class='spanName'> "+
+	item.getName()+"</span>"+
+	"<br> Speed: "+item.getPosition().s+" km/h"+
+	"<br> Last date: "+date+
+	"<br> <span class='spanPos'>X: "+
+	item.getPosition().x+
+	"<br> Y: "+item.getPosition().y+
+	"</span></div>";
+	return container;
+}
+
+function popUp(item) {
+	var date = wialon.util.DateTime.formatTime(item.getPosition().t);
+	var popup = "<div>"+"<span class='spanName'> "+
+	item.getName()+"</span>"+
+	"<br> Speed: "+item.getPosition().s+" km/h"+
+	"<br> Last date: "+date+
+	"<br> <span class='spanPos'>X: "+
+	item.getPosition().x+
+	"<br> Y: "+item.getPosition().y+
+	"</span></div>";
+	return popup;
+}
+
+function addIcon(item) {
+	var myIcon = L.icon({
+	    iconUrl: item.getIconUrl(64),
+	    iconRetinaUrl: item.getIconUrl(64),
+	    iconSize: [32, 32],
+	    iconAnchor: [-16, -16],
+	    popupAnchor: [32, 16]
+	});
+	return myIcon;
 }
